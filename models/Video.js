5cd22d31e07c4c55a7673bb0047da6f95b013a75
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2025-01-07 11:55:56 +0530

    Update Video.js

--- models/Video.js
+++ models/Video.js
@@ -3,33 +3,5 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 
 
-const videoSchema = new Schema({
-    videoId: { type: String, required: true },
-    title: { type: String, required: true },
-    filename: { type: String, required: true },
-    uid: { type: String, required: true, unique: true },
-    description: { type: String, default: '' },
-    likes: [{ type: Schema.Types.ObjectId, ref: "Channel" }],
-    isDraft: { type: Boolean, default: true },
-    isShort: { type: Boolean, default: false },
-    dislikes: [{ type: Schema.Types.ObjectId, ref: "Channel" }],
-    commentsStatus: { type: Boolean, default: true },
-    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
-    tags: [{ type: Schema.Types.ObjectId, ref: 'Tag' }],
-    hashTags: [{ type: Schema.Types.ObjectId, ref: 'Tag' }],
-    uploadDate: { type: Date },
-    length: { type: Number },
-    aspect: { type: Number },
-    category: { type: String },
-    privacySettings: {
-        type: String,
-        trim: true,
-        default: 'private',
-        enum: ['public', 'unlisted', 'private']
-    },
-    viewsEnabled: { type: Boolean, default: true },
-    status: { type: String, trim: true, default: 'Uploading' },
-    channel: { type: Schema.Types.ObjectId, ref: "Channel" }
 
-}, { timestamps: true })
 
